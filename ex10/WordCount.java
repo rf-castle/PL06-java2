f773cf37351fd9466e62af6f84d436d0f397b098
rf-castle <kouta.tashiro.2000@gmail.com>
2021-05-26 23:20:34 +0900

    modify ex10

--- ex10/WordCount.java
+++ ex10/WordCount.java
@@ -5,10 +5,17 @@ import java.util.function.*;
 import java.util.stream.*;
 
 public class WordCount {
-    public static void main(String[] args) {
-        File file = new File("text.txt");
-
-        Stream<String> content_stream = Stream.of();
+    public static void main(String[] args)  {
+        File file = new File(args[0]);
+        Stream<String> content_stream;
+        try {
+            content_stream = Files.lines(file.toPath());
+        }
+        catch (java.io.IOException e){
+            e.printStackTrace();
+            return;
+        }
         Map<String, Long> wc_map = content_stream.flatMap(line-> Arrays.stream(line.split(" "))).filter(x->x.matches("[a-zA-Z]+") && x.length() > 0).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        wc_map.forEach((key, value)-> System.out.printf("%s: %d%n", key, value));
     }
 }
